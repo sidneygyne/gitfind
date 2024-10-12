50c237c72cd8fdcfdbe2650741a3e506e2ff214a
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2024-10-12 18:40:51 -0300

    perfil

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,6 +13,20 @@ function App() {
             <input name="usuario" placeholder="@username"></input>
             <button>Buscar</button>
           </div>
+            <div className="perfil">
+              <img src="https://avatars.githubusercontent.com/u/155782406?v=4" 
+                   className="profile" 
+                   alt="profile"/>
+              <div>
+                <h3>Sidney Rodrigues</h3>
+                <span>Span</span>
+                <p>Descrição</p>
+              </div>
+            </div>
+            <hr/>
+            <div>
+              <h4>Repositórios</h4>
+            </div>
         </div>
       </div>
     </div>
