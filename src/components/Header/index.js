ba610594372b32e2f078d2d2ee29f6b6abdac483
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2024-10-11 21:18:27 -0300

    header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>GitFinf</h1>
+    </header>
+  )
+}
+
+export {Header};
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,10 @@
+import { Header } from "../../components/Header";
 
 
 function App() {
   return (
     <div className="App">
-     <h1>Olá mundo</h1> 
+     <Header/>
     </div>
   );
 }
